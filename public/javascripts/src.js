746bf0b729178873ac4997919c9b74521f859281
negerekvar <volqii@googlemail.com>
2018-05-04 01:54:57 +0300

    changed icons

--- public/javascripts/src.js
+++ public/javascripts/src.js
@@ -1,44 +1,19 @@
-let opts = {
-    transparent: false,
-    backgroundColor: 0x49DCB1,
-    resolution: 1,
-    antialias: 0,
-    autoResize: true,
-    forceCanvas: false,
-    clearBeforeRender: true
-};
-var app = new PIXI.Application(innerWidth, innerHeight, opts);
-
-let _width = app.renderer.width;
-let _height = app.renderer.height;
-var container = new PIXI.particles.ParticleContainer(20000, {
-    scale: true,
-    position: true,
-    rotation: true,
-    uvs: true,
-    alpha: true
-});
-container.interactive = true;
-var basicText = new PIXI.Text('Basic text in pixi', { fontFamily: 'Arial', fontSize: 24, fill: 0xffffff, align: 'center', dropShadow: true, fontWeight: "bolder", dropShadowDistance: 2 });
-basicText.x = 10;
-basicText.y = 0;
-let isAdding = false;
-var mousePos = {
-    x: 0,
-    y: 0
-}
-
-app.stage.addChild(container);
-app.stage.addChild(basicText);
-
-$("#myContainer").append(app.view);
+$(document).ready(onReady)
 window.onresize = resize;
-
-function resize() {
-    app.renderer.resize(innerWidth, innerHeight);
-    _width = app.renderer.width;
-    _height = app.renderer.height;
-}
+let opts = {
+        transparent: false,
+        backgroundColor: 0x49DCB1,
+        resolution: 1,
+        antialias: 0,
+        autoResize: true,
+        forceCanvas: false,
+        clearBeforeRender: true
+    },
+    app,
+    emos = [],
+    bText = new PIXI.Texture.fromImage("images/spritesheet2.png"),
+    container, isAdding = false,
+    mousePos = { x: 0, y: 0 };
 document.addEventListener('touchstart', onTouchStart, true)
 document.addEventListener('touchstart', onTouchMove, true)
 document.addEventListener('touchend', onTouchEnd, true)
@@ -77,42 +52,65 @@ function onMouseUp(event) {
     isAdding = false
 }
 
-class emo extends PIXI.Sprite {
-    constructor(x = Math.random() * app.renderer.width, y = Math.random() * app.renderer.height) {
-        super(PIXI.Texture.fromImage('images/spritesheet.png'));
-        this.anchor.set(.5);
-        this.scale.set(.25 + Math.random() * .3);
-        this.x = x;
-        this.y = y;
-        this.angle = Math.random() * 2 * Math.PI;
-        this.vel = { x: Math.cos(this.angle), y: Math.sin(this.angle) };
-        this.r = Math.random() * .01;
-    }
-    where() {
-        this.x = (this.x > _width + 10) ? 0 : this.x;
-        this.x = (this.x < -10) ? _width : this.x;
-        this.y = (this.y > _height + 10) ? 0 : this.y;
-        this.y = (this.y < -10) ? _height : this.y;
-    }
-    update() {
-        this.x += this.vel.x;
-        this.y += this.vel.y;
-        this.rotation += this.r;
-    }
-}
-for (var a = 0; a < 10; a++) {
-    container.addChild(new emo());
-}
-app.ticker.add(() => {
-    basicText.text = container.children.length + " : " + app.ticker.FPS;
-    if (isAdding) {
-        for (var a = 0; a < 20; a++) {
+function onReady() {
+    app = new PIXI.Application(innerWidth, innerHeight, opts);
+    $("#myContainer").append(app.view);
 
-            container.addChild(new emo(mousePos.x, mousePos.y));
+    // let container = new PIXI.Container();
+    container = new PIXI.particles.ParticleContainer(10000, {
+        scale: false,
+        position: true,
+        rotation: false,
+        uvs: false,
+        alpha: false
+    });
+    container.interactive = true;
+
+    app.stage.addChild(container);
+    for (let i = 0; i < 49; i++) {
+        emos.push(new PIXI.Texture(bText.baseTexture, new PIXI.math.Rectangle(i * 64, 0, 64, 64)))
+    }
+
+    for (var i = 0; i < 200; i++) {
+        let emoji = new PIXI.Sprite(emos[~~(Math.random() * emos.length)]);
+        emoji.x = Math.random() * innerWidth;
+        emoji.y = Math.random() * innerHeight;
+        emoji.scale.set(.5);
+        emoji.rotation = Math.random() * 2 * Math.PI;
+        emoji.angle = Math.random() * 2 * Math.PI;
+        emoji.anchor.set(.5);
+        emoji.vel = { x: 2 * Math.random() * Math.cos(emoji.angle), y: 2 * Math.random() * Math.sin(emoji.angle) };
+        container.addChild(emoji);
+    }
+    app.ticker.add(() => {
+        if (isAdding) {
+            for (var a = 0; a < 5; a++) {
+                let emoji = new PIXI.Sprite(emos[~~(Math.random() * emos.length)]);
+                emoji.x = mousePos.x;
+                emoji.y = mousePos.y;
+                emoji.scale.set(.5);
+                emoji.rotation = Math.random() * 2 * Math.PI;
+                emoji.angle = Math.random() * 2 * Math.PI;
+                emoji.anchor.set(.5);
+                emoji.vel = { x: Math.random() * Math.cos(emoji.angle), y: Math.random() * Math.sin(emoji.angle) };
+                container.addChild(emoji);
+
+            }
         }
-    }
-    for (let i of container.children) {
-        i.update();
-        i.where();
-    }
-});
+        for (let i of container.children) {
+            i.position.x += i.vel.x;
+            i.position.y += i.vel.y;
+            (i.position.x > innerWidth) && (i.position.x = 0);
+            (i.position.x < 0) && (i.position.x = innerWidth);
+            (i.position.y > innerHeight) && (i.position.y = 0);
+            (i.position.y < 0) && (i.position.y = innerHeight);
+
+
+        }
+    });
+
+}
+
+function resize() {
+    app.renderer.resize(innerWidth, innerHeight);
+}
