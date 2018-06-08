c126bb4d1122eb745933aa95d6f94db6375c8b94
volkanozcan2 <vozcan86@googlemail.com>
2018-06-08 14:22:45 +0300

    0xffacb7

--- public/javascripts/src.js
+++ public/javascripts/src.js
@@ -29,26 +29,26 @@ container = new PIXI.Container(opts);
 app.stage.interactive = true;
 app.stage.buttonMode = true;
 app.stage.addChild(container);
-container.hitArea = new PIXI.Rectangle(0, 0, app.renderer.width, app.renderer.height);
-container.on('pointerdown', (event) => {
+app.stage.hitArea = new PIXI.Rectangle(0, 0, app.renderer.width, app.renderer.height);
+app.stage.on('pointerdown', (event) => {
     isAdding = !isAdding;
 });
-container.on('pointerup', (event) => {
+app.stage.on('pointerup', (event) => {
     isAdding = !isAdding;
 });
-container.on('pointerupoutside', (event) => {
+app.stage.on('pointerupoutside', (event) => {
     isAdding = !isAdding;
 });
-container.on('pointermove', (event) => {
+app.stage.on('pointermove', (event) => {
     eventPos.x = event.data.global.x;
     eventPos.y = event.data.global.y;
 
 });
-let filter = new PIXI.filters.OldFilmFilter();
-let filter2 = new PIXI.filters.DropShadowFilter();
+// let filter = new PIXI.filters.OldFilmFilter();
+// let filter2 = new PIXI.filters.DropShadowFilter();
 // let filter3 = new PIXI.filters.GlitchFilter();
 // filter3.fillMode = "LOOP";
-app.stage.filters = [filter2];
+// app.stage.filters = [filter];
 
 for (let i = 0; i < 49; i++) {
     emos.push(new PIXI.Texture(bText.baseTexture, new PIXI.math.Rectangle(i * 64, 0, 64, 64)))
@@ -60,7 +60,7 @@ for (var i = 0; i < 1000; i++) {
 app.ticker.add(() => {
 
     if (isAdding) {
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < 3; i++) {
             container.addChild(new mover(emos[~~(Math.random() * emos.length)], eventPos.x, eventPos.y));
         }
     }
