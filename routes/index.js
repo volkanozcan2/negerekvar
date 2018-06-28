dda7b4eaf5a94877954d7bf117013ee0341a831a
volkanozcan2 <vozcan86@googlemail.com>
2018-06-28 21:43:57 +0000

    deleted comments

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,12 @@ var router = express.Router();
 var isprime = require("../bin/isprime");
 var fs = require("fs");
 /* GET home page. */
-var file = fs.createWriteStream("./comments", { flags: 'a' });
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Jovanni' });
 });
 router.post('/', function(req, res, next) {
     var num = parseInt(req.body.num);
     console.log(req.body);
-    file.write(JSON.stringify(req.body) + "\n");
     res.send(isprime.isPrime(num));
 
 });
